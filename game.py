7f851c236e60d3eb348c054e3a3077e76c23903c
edkotkas <edkotkas@gmail.com>
2015-05-02 19:59:14 +0100

    added music, startup screen and added temporary text

--- game.py
+++ game.py
@@ -1,10 +1,15 @@
 import pygame
 import sys
 import time
+import webbrowser
+
+import os
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 from blocks import Blocks
 
-GRID_ENABLED = False
+GRID_ENABLED = True
 
 
 class Game:
@@ -13,7 +18,11 @@ class Game:
         Falling PyBlocks, a clone of Tetris.
         :return:
         """
+        pygame.mixer.pre_init(44100, -16, 2, 2048)
         pygame.init()
+
+        # sound file downloaded from (https://archive.org/details/TetrisThemeMusic)
+        pygame.mixer.music.load('tetris.ogg')
 
         # grid divider
         self.grid_x = 10
@@ -21,12 +30,14 @@ class Game:
 
         # game speed
         self.game_speed = 0.5
+        self.fps = 30
 
         # game status
         self.over = False
 
         # colours
         self.colour_clear = (25,) * 3
+        self.colour_grid = (50,) * 3
 
         # window setup
         self.window_width = 460
@@ -54,22 +65,31 @@ class Game:
             (4, 0)
         )
 
-        self.top_players = {1: ('Player', 1000)}
+        self.top_players = {
+            1: ('Player1', 1000),
+            2: ('Player2', 140),
+            3: ('Player3', 102),
+            4: ('Player4', 98),
+            5: ('Player5', 1)
+        }
 
-    def loop(self, frames):
+    def loop(self):
         """
         Main game loop.
-        :param frames: integer to represent how many frames to display per second
         :return: main game loop
         """
         shape_current = None
         direction = None
         start_time = time.time()
         move_time = 0
+        paused = False
+
+        pygame.mixer.music.play()
+
         while True:
 
             # fps
-            pygame.time.Clock().tick(frames)
+            pygame.time.Clock().tick(self.fps)
 
             # clear screen
             self.screen.fill(self.colour_clear)
@@ -98,36 +118,71 @@ class Game:
                         # rotate the block shape clockwise
                         self.blocks.rotate()
 
+                    if event.key == pygame.K_m:
+                        if pygame.mixer.music.get_busy():
+                            pygame.mixer.music.stop()
+                        else:
+                            pygame.mixer.music.play()
+
+                    if event.key == pygame.K_r:
+                        print "reset"
+
+                    if event.key == pygame.K_p:
+                        if paused is True:
+                            paused = False
+                        else:
+                            paused = True
+
                 # if any key released set the game speed to normal
                 if event.type == pygame.KEYUP:
                     self.game_speed = 0.5
                     direction = None
 
             # play area background
-            pygame.draw.rect(self.screen, (20,) * 3, (0, 0, self.display_width, self.display_height))
+            pygame.draw.rect(self.screen, (22,) * 3, (0, 0, self.display_width, self.display_height))
 
             # side panel background
             pygame.draw.rect(self.screen, (25,) * 3, (
                 self.display_width, 0,
-                self.window_width - self.display_width, self.window_height))
+                self.window_width - self.display_width, self.window_height), 3)
 
             # next shape panel
             next_shape = pygame.font.SysFont("monospace", 16)
             next_shape = next_shape.render("Next shape:", 1, (200,) * 3)
-            self.screen.blit(next_shape, (self.display_width+10, 10))
+            self.screen.blit(next_shape, (self.display_width + 5, 10))
 
             # top players panel
             top = pygame.font.SysFont("monospace", 20)
-            top = top.render("Top 10:", 1, (200,) * 3)
-            self.screen.blit(top, (self.display_width + 5, 140))
+            top = top.render("Top 5:", 1, (200,) * 3)
+            self.screen.blit(top, (self.display_width + 5, 150))
             for i, player in enumerate(self.top_players.keys()):
                 multiplier = 20 * i
                 player_name = self.top_players.get(player)[0]
                 player_score = self.top_players.get(player)[1]
 
                 board = pygame.font.SysFont("monospace", 14)
-                board = board.render("%d.%s - %s" % (i + 1, player_name, player_score), 1, (200,) * 3)
-                self.screen.blit(board, (self.display_width + 5, 180 + multiplier))
+                board = board.render("%s - %s" % (player_name, player_score), 1, (200,) * 3)
+                self.screen.blit(board, (self.display_width + 10, 180 + multiplier))
+
+            movement = pygame.font.SysFont("monospace", 14)
+            movement = movement.render("Controls:", 1, (200,) * 3)
+            self.screen.blit(movement, (self.display_width + 5, 320))
+
+            wasd = pygame.font.SysFont("monospace", 14)
+            wasd = wasd.render("Arrows - move", 1, (200,) * 3)
+            self.screen.blit(wasd, (self.display_width + 5, 340))
+
+            music = pygame.font.SysFont("monospace", 14)
+            music = music.render("M - music off/on", 1, (200,) * 3)
+            self.screen.blit(music, (self.display_width + 5, 360))
+
+            reset = pygame.font.SysFont("monospace", 14)
+            reset = reset.render("R - reset", 1, (200,) * 3)
+            self.screen.blit(reset, (self.display_width + 5, 380))
+
+            pause = pygame.font.SysFont("monospace", 14)
+            pause = pause.render("P - pause", 1, (200,) * 3)
+            self.screen.blit(pause, (self.display_width + 5, 400))
 
             # the score text
             pygame.draw.rect(self.screen, (25,) * 3, (
@@ -135,7 +190,7 @@ class Game:
                 self.display_width, self.window_height - self.display_width))
 
             score = pygame.font.SysFont("monospace", 18)
-            score = score.render("Score: %d" % self.score, 1, (200,) * 3)
+            score = score.render("SCORE: %d" % self.score, 2, (255,) * 3)
             self.screen.blit(score, (10, self.display_height))
 
             if GRID_ENABLED is True:
@@ -161,13 +216,13 @@ class Game:
                     # convert the grid coordinates to pixel location
                     shape_x, shape_y = self.pixel(*shape)
 
-                    pygame.draw.rect(self.screen, (100,) * 3, (
-                        shape_x, shape_y,
-                        self.grid_real_x, self.grid_real_y), 3)
-
                     pygame.draw.rect(self.screen, colour, (
                         shape_x, shape_y,
                         self.grid_real_x, self.grid_real_y))
+
+                    pygame.draw.rect(self.screen, (50,) * 3, (
+                        shape_x, shape_y,
+                        self.grid_real_x, self.grid_real_y), 3)
 
             else:
                 self.over = True
@@ -181,13 +236,14 @@ class Game:
             if len(the_blocks) > 0:
                 for shape, colour in the_blocks:
                     block_x, block_y = self.pixel(*shape)
-                    pygame.draw.rect(self.screen, (100,) * 3, (
-                        block_x, block_y,
-                        self.grid_real_x, self.grid_real_y), 3)
 
                     pygame.draw.rect(self.screen, colour, (
                         block_x, block_y,
                         self.grid_real_x, self.grid_real_y))
+
+                    pygame.draw.rect(self.screen, (50,) * 3, (
+                        block_x, block_y,
+                        self.grid_real_x, self.grid_real_y), 3)
 
             # if the bottom line is full, add score
             if self.blocks.line():
@@ -210,9 +266,9 @@ class Game:
                 self.display_width + 12 + (25 * x), 35 + (25 * y), 25, 25
             ))
 
-            pygame.draw.rect(self.screen, (25,) * 3, (
+            pygame.draw.rect(self.screen, (50,) * 3, (
                 self.display_width + 12 + (25 * x), 35 + (25 * y), 25, 25
-            ), 1)
+            ), 3)
 
     def grid(self):
         """
@@ -221,7 +277,7 @@ class Game:
         """
         for column in range(self.grid_x):
             for row in range(self.grid_y):
-                pygame.draw.rect(self.screen, (10,) * 3, (
+                pygame.draw.rect(self.screen, (21,) * 3, (
                     self.grid_real_x * column, self.grid_real_y * row,
                     self.grid_real_x, self.grid_real_y), 1)
 
@@ -231,10 +287,10 @@ class Game:
         :return:
         """
         game_over = pygame.font.SysFont("monospace", 32)
-        game_over = game_over.render("Game Over!", 1, (0,) * 3)
+        game_over = game_over.render("GAME OVER", 2, (255,) * 3)
         self.screen.blit(game_over, ((self.display_width / 2) - 32, self.display_height / 2))
 
-    def pixel(self, x=0, y=0):
+    def pixel(self, x, y):
         """
         Converts grid coordinates to pixel coordinates.
         :param x: int between 0 and 10
@@ -243,6 +299,61 @@ class Game:
         """
         return x * (self.display_width / 10), y * (self.display_height / 20)
 
+    def start_screen(self):
+        """
+        Display the startup screen.
+        :return:
+        """
+        button = None
+        creator = None
+        while True:
+
+            # fps
+            pygame.time.Clock().tick(self.fps)
+
+            # clear screen
+            self.screen.fill(self.colour_clear)
+
+            for event in pygame.event.get():
+
+                # exit if X is pressed
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_ESCAPE:
+                        sys.exit()
+
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    mouse_pos = pygame.mouse.get_pos()
+
+                    if button.collidepoint(mouse_pos):
+                        return False
+
+                    if creator.collidepoint(mouse_pos):
+                        webbrowser.open("https://github.com/edkotkas")
+
+            title = pygame.font.SysFont("monospace", 32)
+            title = title.render("Falling PyBlocks", 2, (255,) * 3)
+            self.screen.blit(title, ((self.window_width / 2) - 150, self.window_height / 8))
+
+            play = pygame.font.SysFont("monospace", 32)
+            play = play.render("PLAY", 2, (255,) * 3)
+
+            self.screen.blit(play, ((self.window_width / 2) - 40, self.window_height / 2))
+
+            button = pygame.draw.rect(self.screen, (255,) * 3, (
+                (self.window_width / 2) - 50, self.window_height / 2, 95, 35
+            ), 2)
+
+            creator = pygame.font.SysFont("monospace", 14)
+            creator = creator.render("Falling PyBlocks by Eduard Kotkas (GitHub - @edkotkas)", 2, (255,) * 3)
+            creator = self.screen.blit(creator, (5, self.window_height - 18))
+
+            pygame.display.flip()
+
 if __name__ == "__main__":
     fpb = Game()
-    fpb.loop(30)
+    fpb.start_screen()
+    fpb.loop()
